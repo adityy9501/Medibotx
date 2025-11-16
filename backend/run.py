8dea1cf8bf60b071482dc40e657f7a8926ac267b
aditya hurde <140227379+adityy9501@users.noreply.github.com>
2025-11-16 20:42:49 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import uvicorn
 import multiprocessing
 
@@ -13,19 +13,4 @@ if __name__ == "__main__":
         reload=False, 
         workers=workers  
     )
-=======
-import uvicorn
-import multiprocessing
 
-if __name__ == "__main__":
-    
-    workers = multiprocessing.cpu_count()
-    
-    uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=8007,
-        reload=False, 
-        workers=workers  
-    )
->>>>>>> 2b90058878e36dd5e29c9c8d4eb7ea0df45f5dec
